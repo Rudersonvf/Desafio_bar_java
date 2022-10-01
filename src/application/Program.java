4d61debb561c11f5c5c5455178c491f17993a3a5
Ruderson Florentino <rudersonvf@gmail.com>
2022-10-01 17:11:04 -0300

    Implemented Bill class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Bill;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+	
+		
+		scan.close();
 	}
 
 }
--- src/entities/Bill.java
+++ src/entities/Bill.java
@@ -2,5 +2,53 @@ package entities;
 
 public class Bill {
 	
+	public final double MIN_FEEDING = 30.0;
 
+	public char gender;
+	public int beer;
+	public int barbecue;
+	public int softDrink;
+	
+	public double cover() {
+		if(feeding() > MIN_FEEDING) {
+			return 0.0;
+		}
+		else {
+			return 4.0;
+		}
+	}
+	
+	public double feeding() {
+		return beer + barbecue + softDrink;
+	}
+	
+	public double ticket() {
+		if(gender == 'M') {
+			return 10.0;
+		}
+		else {
+			return 8.0;
+		}
+	}
+	
+	public double total() {
+		return cover() + feeding() + ticket();
+	}
+	
+	public String toString() {
+		if(feeding() > MIN_FEEDING) {
+			return String.format("RELATÓRIO: %n")
+					+ String.format("Consumo = R$ %.2f%n", feeding())
+					+ String.format("Isento de Couvert %n")
+					+ String.format("Ingresso = R$ %.2f%n%n", ticket())
+					+ String.format("Valor a pagar = R$ %.2f%n", total());
+		}
+		else {
+			return String.format("RELATÓRIO: %n")
+					+ String.format("Consumo = R$ %.2f%n", feeding())
+					+ String.format("Couvert = R$ %.2f%n", cover())
+					+ String.format("Ingresso = R$ %.2f%n%n", ticket())
+					+ String.format("Valor a pagar = R$ %.2f%n", total());
+		}
+	}
 }
